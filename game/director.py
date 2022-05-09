3fa87c3795d5c1b9de21916f22ddf04da3ae6d4a
hmosier <hannah89mosier@byui.edu>
2022-05-09 15:23:12 -0400

    some updates to the director class

--- game/director.py
+++ game/director.py
@@ -4,7 +4,7 @@
     Rules listed and your program meets all of the Requirements found on 
     https://byui-cse.github.io/cse210-course-competency/abstraction/materials/hilo-specification.html
 """
-
+from game.card import cards
 
 class Director:
     """A person who directs the game. 
@@ -12,7 +12,10 @@ class Director:
     The responsibility of a Director is to control the sequence of play.
 
     Attributes:
+        cards (List[cards]): a list of cards instances.
         is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
     """
 
     def __init__(self):
@@ -21,7 +24,14 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        self.cards = []
         self.is_playing = True
+        self.score = 0
+        self.total_score = 0
+
+        for i in range(1):
+            card = cards()
+            self.cards.append(card)
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -35,12 +45,14 @@ class Director:
             self.do_outputs()
 
     def get_inputs(self):
-        """Ask the user if they want to roll.
+        """Ask the user if they want to draw a card.
 
         Args:
             self (Director): An instance of Director.
         """
-        pass
+        draw_card = input("Higher or lower? [h/l] ")
+        self.is_playing = (draw_card == "y")
+
 
     def do_updates(self):
         """Updates the player's score.
